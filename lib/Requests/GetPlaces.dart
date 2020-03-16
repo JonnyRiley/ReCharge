5f4aa6ecf6be4d2982cae36fb4bbb81b27f4001c
Sarah Baines <sarahbaines@Sarahs-MacBook-Pro-2.local>
2020-03-16 11:20:22 +0000

    add dropdown and filter fetch data by place type

--- lib/Requests/GetPlaces.dart
+++ lib/Requests/GetPlaces.dart
@@ -2,19 +2,17 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-// void main() {
-//   fetchPlaces();
-// }
-
-Future<List<dynamic>> fetchPlaces() async {
+Future<List<dynamic>> fetchPlaces(String placeType) async {
   var uri =
       Uri.https('maps.googleapis.com', '/maps/api/place/nearbysearch/json', {
     "location": "53.483959,-2.244644",
     "radius": "500",
-    "types": "cafe",
+    "type": placeType,
+    // "rankby": "distance",
     "key": "AIzaSyB9HqRXH7PlvczMIg4LgUYBR4oxMI8M7fs"
   });
   final response = await http.get(uri);
+  print(response.body);
   if (response.statusCode == 200) {
     var listOfPlaces = jsonDecode(response.body)['results'];
     var listOfPlaceObjects =
@@ -29,18 +27,15 @@ class Place {
   final double latitude;
   final double longitude;
   final String name;
-  final dynamic photoRef;
-  final double rating;
+  final String icon;
+  final num rating;
 
-  const Place(
-      this.latitude, this.longitude, this.name, this.rating, this.photoRef);
+  const Place(this.latitude, this.longitude, this.name, this.rating, this.icon);
 
   Place.fromUnformatted(Map place)
       : latitude = place["geometry"]["location"]["lat"],
         longitude = place["geometry"]["location"]["lng"],
         name = place["name"],
-        photoRef = place["photos"] != null
-            ? place["photos"][0]["photo_reference"]
-            : null,
+        icon = place["icon"],
         rating = place["rating"];
 }
--- lib/Widgets/ChargePointInfoPage.dart
+++ lib/Widgets/ChargePointInfoPage.dart
@@ -8,18 +8,48 @@ class ChargePointInfoPage extends StatefulWidget {
 }
 
 class InfoPageState extends State<ChargePointInfoPage> {
+  String dropdownValue = 'cafe';
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: fetchPlaces(),
+        future: fetchPlaces(dropdownValue),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return Scaffold(
               appBar: MainHeader(
-                title: Text("Chargy"),
+                title: Text("reCharge"),
               ),
-              body: ListView(
-                children: createPlaceTileList(snapshot.data),
+              body: Column(
+                children: <Widget>[
+                  DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: TextStyle(color: Colors.deepPurple),
+                      underline: Container(height: 2, color: Colors.deepPurple),
+                      onChanged: (String newValue) {
+                        setState(() => {dropdownValue = newValue});
+                      },
+                      items: <String>[
+                        'cafe',
+                        'museum',
+                        'restaurant',
+                        'atm',
+                        'book_store',
+                        'movie_theatre'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList()),
+                  Expanded(
+                    child: ListView(
+                      children: createPlaceTileList(snapshot.data),
+                    ),
+                  ),
+                ],
               ),
             );
           } else {
@@ -43,7 +73,10 @@ List<Widget> createPlaceTileList(placeList) {
           shape: BoxShape.rectangle,
           borderRadius: BorderRadius.circular(17.5)),
       child: ListTile(
-          title: Text(place.name, style: ,), subtitle: Text("${place.rating} out of 5")),
+          title: Text(place.name),
+          subtitle: Text(
+              place.rating == null ? "no ratings" : "${place.rating} out of 5"),
+          trailing: Image.network(place.icon)),
     );
     placeTileList.add(currentTile);
   });
